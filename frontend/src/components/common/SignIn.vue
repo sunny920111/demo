238332a04152903df354cee66c3aa9bf5ca8d831
sunny920111 <sunny920111@naver.com>
2023-10-16 10:35:39 +0900

    로그인 화면 생성

---
+++ frontend/src/components/common/SignIn.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="container" class="container">
+    <!-- FORM SECTION -->
+    <div class="row">
+      <div class="col align-items-center flex-col"></div>
+      <!-- SIGN IN -->
+      <div class="col align-items-center flex-col sign-in">
+        <div class="form-wrapper align-items-center">
+          <div class="form sign-in">
+            <div class="input-group">
+              <i class='bx bxs-user'></i>
+              <input type="text" placeholder="Username">
+            </div>
+            <div class="input-group">
+              <i class='bx bxs-lock-alt'></i>
+              <input type="password" placeholder="Password">
+            </div>
+            <button>
+              Sign in
+            </button>
+            <p>
+              <b>
+                Forgot password?
+              </b>
+            </p>
+            <p>
+							<span>
+								Don't have an account?
+							</span>
+              <b onclick="toggle()" class="pointer">
+                Sign up here
+              </b>
+            </p>
+          </div>
+        </div>
+        <div class="form-wrapper">
+
+        </div>
+      </div>
+      <!-- END SIGN IN -->
+    </div>
+    <!-- END FORM SECTION -->
+
+    <!-- CONTENT SECTION -->
+    <div class="row content-row">
+      <!-- SIGN IN CONTENT -->
+      <div class="col align-items-center flex-col">
+        <div class="text sign-in">
+          <h2>
+            Welcome~!
+          </h2>
+
+        </div>
+        <div class="img sign-in">
+
+        </div>
+      </div>
+      <!-- END SIGN IN CONTENT -->
+      <div class="col align-items-center flex-col"></div>
+    </div>
+    <!-- END CONTENT SECTION -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  mounted() {
+    let container = document.getElementById('container');
+    setTimeout(() => {
+      container.classList.add('sign-in')
+    }, 200)
+  },
+  methods: {
+    toggle() {
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
